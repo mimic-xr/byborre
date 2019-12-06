525361fe7e3be284f3d09fcc710ae38c480665c2
Elliott_Mimic Workstation <elliott@mimicxr.com>
2019-12-06 14:29:08 +0000

    First Draft UI

---
+++ Byborre_Unity/Assets/M-XR/Scripts/FabricManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FabricManager : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    [System.Serializable]
+    public class Pattern
+    {
+        public GameObject[] Pannel;
+        public Material[] Fabric;
+        public GameObject[] UItarget;
+        public GameObject BoxTarget;
+    }
+    public Pattern[] Patterns;
+    public GameObject[] FabricBox;
+    public Material[] FabricBox_mats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<LineRenderer>().enabled = true;
+        lineRenderer = GetComponent<LineRenderer>();
+        GetComponent<LineRenderer>().enabled = false;
+        foreach (GameObject box in FabricBox)
+        {
+            box.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetMaterial(int pannel_index,int fabric_index)
+    {
+        //Changes the pannel to the the new material
+        foreach (GameObject fabric in Patterns[pannel_index].Pannel)
+        {
+            fabric.GetComponent<Renderer>().material = Patterns[pannel_index].Fabric[fabric_index];
+        }
+        //changes the fabric box to the new material
+        FabricBox[pannel_index].transform.GetChild(0).gameObject.GetComponent<Renderer>().material = FabricBox_mats[fabric_index];
+        foreach(GameObject Box in FabricBox)
+        {
+            Box.SetActive(false);
+        }
+    }
+    public void UpdateLines(int index)
+    {
+        lineRenderer.SetPosition(0, Patterns[index].BoxTarget.GetComponent<Transform>().position);
+        lineRenderer.SetPosition(1, Patterns[index].UItarget[0].GetComponent<Transform>().position);
+    }
+}
---
+++ Byborre_Unity/Assets/M-XR/Scripts/FabricPatch.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FabricPatch : MonoBehaviour
+{
+    public int Index;
+    public GameObject Manager;
+    public Vector3 Startp;
+    public Quaternion Sangle;
+    void Start()
+    {
+        Startp = GetComponent<Transform>().transform.position;
+        Sangle = GetComponent<Transform>().transform.rotation;
+        Manager = GameObject.Find("FabricManager");
+    }
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name=="MaterialBox")
+        {
+            int ID = other.GetComponent<MatBox>().Index;
+            Manager.GetComponent<FabricManager>().SetMaterial(ID, Index);
+            GetComponent<Transform>().transform.position = Startp;
+            GetComponent<Transform>().transform.rotation = Sangle;
+        }
+        if(other.name=="Target")
+        {
+            Manager.GetComponent<LineRenderer>().enabled = true;
+            int ID = other.GetComponent<Transform>().parent.gameObject.GetComponent<MatBox>().Index;
+            Manager.GetComponent<FabricManager>().UpdateLines(ID);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Target")
+        {
+            int ID = other.GetComponent<Transform>().parent.gameObject.GetComponent<MatBox>().Index;
+            Manager.GetComponent<LineRenderer>().enabled = false;
+        }
+        if (other.name == "FabricZone")
+        {
+            foreach(GameObject box in Manager.GetComponent<FabricManager>().FabricBox)
+            {
+                box.SetActive(true);
+            }    
+        }
+    }
+}
