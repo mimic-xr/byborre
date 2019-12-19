3e9fdf2467f8a33a4f1a9a1d3f7a49173b8df983
Elliott Laptop <Elliott@mimicxr.com>
2019-12-19 03:00:36 +0000

    Working Build

--- Byborre_Unity/Assets/M-XR/Scripts/SceneReset.cs
+++ Byborre_Unity/Assets/M-XR/Scripts/SceneReset.cs
@@ -5,13 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class SceneReset : MonoBehaviour
 {
+    public float Wait_Time;
+    public float timer;
+    public bool No_Activity=false;
+
+    private void Update()
+    {
+        if(No_Activity==true)
+        {
+            timer += Time.deltaTime;
+            if(timer>Wait_Time)
+            {
+                No_Activity = false;
+                timer = 0;
+                Reset();
+            }
+        }
+        if(No_Activity==false)
+        {
+            timer = 0;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         Reset();
     }
     private void Reset()
     {
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
