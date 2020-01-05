dfe2a28278aec244d1e2059c5149f6235da3f5a7
Elliott Laptop <Elliott@mimicxr.com>
2020-01-05 16:57:23 +0000

    Updates in NYC

---
+++ Byborre_Unity/Assets/M-XR/Scripts/HandDetector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandDetector : MonoBehaviour
+{
+    public bool touching;
+    public bool started;
+    public SceneReset S_Reset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.layer==LayerMask.NameToLayer("Hands"))
+        {
+            S_Reset.No_Activity = false;
+            started = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Hands"))
+        {
+            S_Reset.No_Activity = true;
+        }
+    }
+}
